6f6a3de68dc092abec5feaf69917e070b7d9bd41
David Wray <magyverwray@gmail.com>
2016-04-14 14:33:28 -0700

    Initial

---
+++ Assets/Drag.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class Drag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+    public Transform parentToReturnTo = null;
+
+    public enum Zone { CREATURE, DECKMASTER, EVENT, HAND };
+    public Zone typeOfCard = Zone.CREATURE;
+
+	public void OnBeginDrag(PointerEventData pointerData)
+    {
+        parentToReturnTo = this.transform.parent;
+        this.transform.SetParent(this.transform.parent.parent);
+
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData pointerData)
+    {
+        this.transform.position = pointerData.position;
+    }
+
+    public void OnEndDrag(PointerEventData pointerData)
+    {
+        this.transform.SetParent(parentToReturnTo);
+
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+
+    }
+}
---
+++ Assets/Drop.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class Drop : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler {
+
+    public Drag.Zone typeOfCard = Drag.Zone.HAND;
+    
+    public void OnPointerEnter(PointerEventData pointerData)
+    {
+
+    }
+
+    public void OnPointerExit(PointerEventData pointerData)
+    {
+
+    }
+
+
+    public void OnDrop(PointerEventData pointerData)
+    {
+        //Debug.Log(pointerData.pointerDrag.name + "was dropped on " + gameObject.name);
+
+        Drag d = pointerData.pointerDrag.GetComponent<Drag>();
+
+        if (d != null)
+        {
+            if (typeOfCard == d.typeOfCard || typeOfCard == Drag.Zone.HAND)
+            {
+                d.parentToReturnTo = this.transform;
+            }
+        }
+    }
+}
